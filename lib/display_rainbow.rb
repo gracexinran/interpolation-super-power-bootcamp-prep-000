7aa702caceca0195773926661321bead4bbd2027
Xinran Che <gracexinran@gmail.com>
2019-05-01 06:43:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,9 @@
 def display_rainbow(colors)
   rainbow = []
   7.times do |i|
-    rainbow.push("#{colors[i][0].uppercase") 
+    rainbow.push("#{colors[i][0].upcase}: #{colors[i]}")
+  end
+  puts "#{rainbow.join(", ")}"
+end
+
+colors = []
